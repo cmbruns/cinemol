8f7c6e7eba9d4018526bb83eb11cd7aaef0255eb
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@2e4361bc-f7a3-a45f-efc4-e391e576046a>
2012-07-29 14:16:10 +0000

    Created commands and console_context modules for python console. Created commands "refresh()", "center()" and property "focus".

---
+++ trunk/src/cinemol/console_context.py
@@ -0,0 +1,11 @@
+'''
+Created on Jul 29, 2012
+
+@author: cmbruns
+'''
+
+# This module gets populated programmatically
+cm = None # cm will be replace with a CommandContext by CinemolApp()
+
+def _context():
+    return globals()
