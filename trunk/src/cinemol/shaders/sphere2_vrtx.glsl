fc7ae24a677fa79d5b4d9b3f91f744f7b837f262
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@2e4361bc-f7a3-a45f-efc4-e391e576046a>
2012-08-26 15:00:27 +0000

    Partial progress toward new "QuadScene" to test glsl 1.5 and geometry shader.

--- trunk/src/cinemol/shaders/sphere2_vrtx.glsl
+++ trunk/src/cinemol/shaders/sphere2_vrtx.glsl
@@ -1,17 +1,23 @@
-#version 120
+#version 150
+
+in vec3 vertexPosition;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+invariant gl_Position;
 
 uniform float radius_scale;
 uniform float radius_offset;
 uniform int imposter_edge_count;
 
-varying vec3 sphere_center_in_eye;
-varying float radius;
+out vec3 sphere_center_in_eye;
+out float radius;
 
 void main()
 {
-    radius = gl_Normal.z * radius_scale + radius_offset;
-    vec4 sc = gl_ModelViewMatrix * gl_Vertex;
+    // radius = gl_Normal.z * radius_scale + radius_offset;
+    radius = 1.0;
+    vec4 sc = modelViewMatrix * vec4(vertexPosition, 1.0);
     vec3 sphere_center_in_eye = sc.xyz * 1.0/sc.w;
-    gl_Position = gl_ProjectionMatrix * sc; // in clip
-    gl_FrontColor = gl_Color;
+    gl_Position = projectionMatrix * sc; // in clip
+    gl_FrontColor = vec4(1,0,0,1);
 }
