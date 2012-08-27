80275f68df3b3f745ceb37369c904056f226cd70
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@2e4361bc-f7a3-a45f-efc4-e391e576046a>
2012-08-27 01:36:32 +0000

    Version 150 sphere shader now does   * ray tracing   * primitive halos   * correct depth buffer

--- trunk/src/cinemol/shaders/sphere2_geom.glsl
+++ trunk/src/cinemol/shaders/sphere2_geom.glsl
@@ -3,33 +3,56 @@
 
 // Incoming point vertex is used as the center of a regular
 // polygon, oriented toward the user.
+// These shaders will paint a picture of a sphere on that polygon.
 
 #define numSides 4
 
+const float apothemRatio = cos(3.14159 / numSides); // polygon needs to be larger than circle it encloses
+const float sideTheta = 2.0 * 3.14159 / float(numSides); // angle between polygon vertices
+
 uniform mat4 projectionMatrix;
-in float radius[];
 
 layout (points) in;
 layout (triangle_strip, max_vertices = numSides) out;
+
+in float radius[];
+in float horizonRatio[];
+
+out vec2 positionInImposter;
+out float imposterRadiusSquared;
+out vec3 positionInCamera;
+// Quadratic equation paramers for fragment shader ray tracing
+out float qe_half_b;
+out float qe_c;
 
 void main()
 {
     gl_FrontColor = gl_FrontColorIn[0];
 
-    // Emit points from alternate sides of the polygon,
+    // Emit points from alternate halves of the polygon,
     // to keep the strip triangles in counterclockwise orientation.
-    const float sideTheta = 2.0 * 3.14159 / float(numSides); // angle between vertices
-    float theta = sideTheta/2.0; // angle of vertex in polar coordinates
+    float theta = sideTheta/2.0; // angle of vertex in polar coordinates, align first side with x-axis
     float dTheta = 0.0; // magnitude of next vertex angle change
     float signDTheta = 1.0; // sign of next vertex angle change
+    float atomRadius = radius[0];
+    float imposterRadius = horizonRatio[0] * atomRadius; // perspective horizon bulge
+    float circumradius = imposterRadius / apothemRatio; // polygon corners stick out further
     for(int v = 0; v < numSides; ++v)  // one vertex at a time, starting at top middle
     {   
         theta += signDTheta * dTheta;
         float sinTheta = sin(theta);
         float cosTheta = cos(theta);
-        vec4 delta = radius[0] * vec4(cosTheta, -sinTheta, 0, 0);
-        gl_Position = projectionMatrix * (gl_PositionIn[0] + delta);
+        imposterRadiusSquared = imposterRadius * imposterRadius;
+        positionInImposter = circumradius * vec2(cosTheta, -sinTheta);
+        vec3 delta = vec3(positionInImposter, 0);
+        vec3 sphereCenterInCamera = gl_PositionIn[0].xyz;
+        positionInCamera = sphereCenterInCamera + delta;
+        gl_Position = projectionMatrix * vec4(positionInCamera, 1);
+        qe_half_b = -1.0 * dot(positionInCamera, sphereCenterInCamera);
+        qe_c = dot(sphereCenterInCamera, sphereCenterInCamera) - atomRadius*atomRadius;
+
         EmitVertex();
+        
         signDTheta *= -1.0;
         dTheta += sideTheta;
     }
--- trunk/src/cinemol/shaders/sphere2_vrtx.glsl
+++ trunk/src/cinemol/shaders/sphere2_vrtx.glsl
@@ -1,22 +1,25 @@
 #version 150
 
-in vec3 vertexPosition;
 uniform mat4 modelViewMatrix;
-
 uniform float radius_scale;
 uniform float radius_offset;
-uniform int imposter_edge_count;
 
-out vec4 gl_Position;
-out vec3 sphere_center_in_eye;
+in vec3 vertexPosition;
+
 out float radius;
+out vec4 gl_Position;
+out float horizonRatio; // d/sqrt(d^2-r^2) correction for perpective horizon
 
 void main()
 {
-    // radius = gl_Normal.z * radius_scale + radius_offset;
-    radius = 1.0;
-    vec4 sc = modelViewMatrix * vec4(vertexPosition, 1.0);
-    vec3 sphere_center_in_eye = sc.xyz * 1.0/sc.w;
-    gl_Position = sc; // not in clip yet
-    gl_FrontColor = vec4(1,0.5,0.5,1);
+    gl_FrontColor = vec4(0.4, 0.1, 0 ,1);
+
+    radius = 1.0 * radius_scale;
+
+    // projection matrix is postponed to geometry shader
+    gl_Position = modelViewMatrix * vec4(vertexPosition, 1.0);
+    
+    // horizon bulges larger than midline disk in perspective projection
+    float cameraDistanceSqr = dot(gl_Position, gl_Position);
+    horizonRatio = sqrt(cameraDistanceSqr / (cameraDistanceSqr - radius*radius));
 }
