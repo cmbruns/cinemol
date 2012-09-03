cc5aa035890bde761efa88eb00d1bd9831e5a953
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@2e4361bc-f7a3-a45f-efc4-e391e576046a>
2012-09-03 12:15:38 +0000

    Dev 150 shader now works with buffer objects.  Was wrongly using "0" instead of "None" in glVertexAttribPointer call.

--- trunk/src/cinemol/shaders/sphere2_vrtx.glsl
+++ trunk/src/cinemol/shaders/sphere2_vrtx.glsl
@@ -15,7 +15,11 @@ void main()
 {
     vec3 atomColorLinear = pow(atomColorSrgb, vec3(2.2)); // approximately
     gl_FrontColor = vec4(atomColorLinear, 1);
+    // gl_FrontColor = vec4(0.5, 0, 0.5, 1.0);
+    
     radius = vdwRadius * radiusScale + radiusOffset;
+    // radius = 1.0 * radiusScale + radiusOffset;
+    
     // projection matrix is postponed to geometry shader
     gl_Position = modelViewMatrix * vec4(atomPosition, 1.0);
 }
