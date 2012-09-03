e7b4c7a802a24db2e48fea6c4c6cb6587f9c55ad
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@2e4361bc-f7a3-a45f-efc4-e391e576046a>
2012-09-03 19:56:12 +0000

    Created untested initial version of shader programs for wireframe bond lines.

--- trunk/src/cinemol/shader.py
+++ trunk/src/cinemol/shader.py
@@ -1,5 +1,7 @@
 from actor import Actor
 from OpenGL.GL import *
+import cinemol.cinemol_globals as cinemol_globals
+import os
 
 
 class ShaderProgram:
@@ -71,4 +73,126 @@ void main()
 }
 """
 
+
+class Shader150:
+    def __init__(self):
+        self.is_initialized = False
+        self.shader_program = 0
+        self.previous_program = 0
+        # Indices for shared vertex attributes
+        # TODO - centralize these indices
+        self.position_attrib = 0
+        self.color_attrib = 1
+        self.radius_attrib = 2
+
+    def __enter__(self):
+        if not self.is_initialized:
+            self.init_gl()
+        self.previous_program = glGetIntegerv(GL_CURRENT_PROGRAM)
+        glUseProgram(self.shader_program)
+        glUniformMatrix4fv(
+                glGetUniformLocation(self.shader_program, "modelViewMatrix"),
+                1, False, glGetDoublev(GL_MODELVIEW_MATRIX).tolist())
+        glUniformMatrix4fv(
+                glGetUniformLocation(self.shader_program, "projectionMatrix"),
+                1, False, glGetDoublev(GL_PROJECTION_MATRIX).tolist())
+        glBindFragDataLocation(self.shader_program, 0, "fragColor");
+        #
+        return self
+
+    def __exit__(self, type, value, tb):
+        glDisableVertexAttribArray(self.position_attrib)
+        glDisableVertexAttribArray(self.color_attrib)
+        glDisableVertexAttribArray(self.radius_attrib)
+        glUseProgram(self.previous_program)
+
+    def init_one_shader(self, file_name, shader_type):
+        this_dir = os.path.split(__file__)[0]
+        shader_dir = os.path.join(this_dir, "shaders")
+        # print os.path.join(shader_dir, file_name)
+        shader_string = open(os.path.join(shader_dir, file_name)).read()
+        shader = glCreateShader(shader_type)
+        glShaderSource(shader, shader_string)
+        glCompileShader(shader)
+        log = glGetShaderInfoLog(shader)
+        if log:
+            print "Shader error:", log
+        return shader
+
+
+class Sphere2Shader(Shader150):
+    def __init__(self):
+        Shader150.__init__(self)
+        # Default values for uniforms
+        self.light_direction = [1.0, 2.0, 1.0];
+        self.radius_scale = 1.0
+        self.radius_offset = 2.0
+
+    def __enter__(self):
+        Shader150.__enter__(self)
+        glUniform1f(glGetUniformLocation(self.shader_program, "radiusScale"), 
+                    self.radius_scale * cinemol_globals.atom_scale)
+        glUniform1f(glGetUniformLocation(self.shader_program, "radiusOffset"), 
+                    self.radius_offset)
+        glUniform3fv(glGetUniformLocation(self.shader_program, "lightDirection"), 
+                    1, self.light_direction)
+        #
+        return self
+    
+    def init_gl(self):
+        if self.is_initialized:
+            return
+        vertex_shader = self.init_one_shader(
+                "sphere2_vrtx.glsl", GL_VERTEX_SHADER)
+        geometry_shader = self.init_one_shader(
+                "sphere2_geom.glsl", GL_GEOMETRY_SHADER)
+        fragment_shader = self.init_one_shader(
+                "sphere2_frag.glsl", GL_FRAGMENT_SHADER)
+        shader_program = glCreateProgram()
+        glAttachShader(shader_program, vertex_shader)
+        glAttachShader(shader_program, geometry_shader)
+        glAttachShader(shader_program, fragment_shader)
+        glBindAttribLocation(shader_program, self.position_attrib, "atomPosition")
+        glBindAttribLocation(shader_program, self.color_attrib, "atomColorLinear")
+        glBindAttribLocation(shader_program, self.radius_attrib, "vdwRadius")
+        glLinkProgram(shader_program)
+        # print shader_program, vertex_shader, fragment_shader
+        log = glGetProgramInfoLog(shader_program)
+        if log:
+            print "Shader program error:", log
+        self.shader_program = shader_program
+        self.is_initialized = True
+    
+
+class WireFrameShader(Shader150):
+    def __init__(self):
+        Shader150.__init__(self)
+    
+    def __enter__(self):
+        Shader150.__enter__(self)
+        return self
+    
+    def init_gl(self):
+        if self.is_initialized:
+            return
+        vertex_shader = self.init_one_shader(
+                "bondline_vrtx.glsl", GL_VERTEX_SHADER)
+        geometry_shader = self.init_one_shader(
+                "bondline_geom.glsl", GL_GEOMETRY_SHADER)
+        fragment_shader = self.init_one_shader(
+                "bondline_frag.glsl", GL_FRAGMENT_SHADER)
+        shader_program = glCreateProgram()
+        glAttachShader(shader_program, vertex_shader)
+        glAttachShader(shader_program, geometry_shader)
+        glAttachShader(shader_program, fragment_shader)
+        glBindAttribLocation(shader_program, self.position_attrib, "atomPosition")
+        glBindAttribLocation(shader_program, self.color_attrib, "atomColorLinear")
+        glBindAttribLocation(shader_program, self.radius_attrib, "vdwRadius")
+        glLinkProgram(shader_program)
+        # print shader_program, vertex_shader, fragment_shader
+        log = glGetProgramInfoLog(shader_program)
+        if log:
+            print "Shader program error:", log
+        self.shader_program = shader_program
+        self.is_initialized = True
 
