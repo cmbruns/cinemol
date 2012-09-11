78f11c8587e684103c330e107d08282e713bba73
Christopher.Bruns@gmail.com <Christopher.Bruns@gmail.com@2e4361bc-f7a3-a45f-efc4-e391e576046a>
2012-09-11 00:56:10 +0000

    Attempt to overcome ATI wireframe problem by rearranging fragment shader.

--- src/cinemol/shaders/bondline_frag.glsl
+++ src/cinemol/shaders/bondline_frag.glsl
@@ -7,11 +7,12 @@ out vec4 fragColor;
 
 void main()
 {
-    // Round end cap at atom
-    if (quadPosition.x > 0.0) {
-        if (dot(quadPosition, quadPosition) > 1.0)
-            discard; // rounded end cap
-    }
     // Pass through color
     fragColor = gl_Color;
+    float posX = quadPosition.x;
+    if (posX <= 0.0)
+        return; // not in end cap area
+    // Round end cap at atom
+    if (dot(quadPosition, quadPosition) > 1.0)
+        discard; // rounded end cap
 }
